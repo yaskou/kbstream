f49dcf6af4cc9d532debe85a990260df4209b758
yaskou <anmitutarow@gmail.com>
2022-09-17 20:17:02 +0900

    feat: Change default theme to dark theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark'
+  }
+})
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
