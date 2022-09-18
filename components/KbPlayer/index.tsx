26f671a4ffd0f28a3ef8025bbdb7de81ba6ed40c
yaskou <anmitutarow@gmail.com>
2022-09-18 15:27:41 +0900

    docs: Add comment

--- components/KbPlayer/index.tsx
+++ components/KbPlayer/index.tsx
@@ -11,10 +11,15 @@ const KbPlayer = ({ src }: Props) => {
 
   return (
     <Box
+      // Control playback state by cursor position
       onMouseEnter={onHover}
       onMouseLeave={onUnHover}
+
+      // Like youtube
       display="flex"
       flexDirection="column"
+
+      // Aspect ratio always is 16:9
       sx={{ aspectRatio: '16 / 9' }}
     >
       <VideoPlayer ref={ref} src={src} />
