ee16b2dd568ae0306d8ee6e897c921e8a1737d55
yaskou <anmitutarow@gmail.com>
2022-09-18 14:48:07 +0900

    feat: Import header component to display some information

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
+import { Toolbar } from '@mui/material'
 import type { NextPage } from 'next'
+import Header from '../components/Header'
 import StreamsGrid from '../components/StreamsGrid'
 
 const Home: NextPage = () => {
   return (
     <>
+      <Header />
+      <Toolbar />
       <StreamsGrid />
     </>
   )
