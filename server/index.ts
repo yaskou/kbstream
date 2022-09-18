2453326d33909f0c1769b9335842ad3402af1237
yaskou <anmitutarow@gmail.com>
2022-09-18 16:01:15 +0900

    docs: Add comment

--- server/index.ts
+++ server/index.ts
@@ -6,26 +6,35 @@ const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
 const port = 3000
 
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const app = next({ dev }) // Accept hot reload in dev mode
+const handle = app.getRequestHandler() // Request handler
 
 app.prepare().then(() => {
+
+  // Create a http server
   const httpServer = createServer(async (req, res) => {
     try {
-      await handle(req, res)
+      await handle(req, res) // handle request
     }
     catch (err) {
+
+      // Server error (500)
       console.error('Error occurred handling', req.url, err)
       res.statusCode = 500
       res.end('internal server error')
     }
-  }).listen(port, (err?: any) => {
-    if (err) throw err
-    console.log(`> Ready on http://${hostname}:${port}`)
   })
 
-  const io = new Server().listen(httpServer)
+    // Start the server
+    .listen(port, (err?: any) => {
+      if (err) throw err
+      console.log(`> Ready on http://${hostname}:${port}`)
+    })
+
+  const io = new Server().listen(httpServer) // Socket.io server with this http server
   io.on('connection', socket => {
+
+    // When base64 is received, broadcast to other everyone
     socket.on('stream', (base64: string) => {
       socket.broadcast.emit('stream', base64)
     })
