b889789b498779a6a78d8ecffd4efeceab5fe049
yaskou <anmitutarow@gmail.com>
2022-09-18 13:42:48 +0900

    perf: Change key to index to avoid unnecessary rendering

--- components/StreamsGrid/index.tsx
+++ components/StreamsGrid/index.tsx
@@ -7,8 +7,8 @@ const StreamsGrid = () => {
 
   return (
     <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 4, md: 6 }}>
-      {base64s.map(base64 => (
-        <Grid item xs={2} sm={2} md={2} key={base64}>
+      {base64s.map((base64, index) => (
+        <Grid item xs={2} sm={2} md={2} key={index}>
           <KbPlayer src={base64} />
         </Grid>
       ))}
