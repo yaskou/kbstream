562092b05357c8a631fd57e4580f5be1663bee5a
yaskou <anmitutarow@gmail.com>
2022-09-18 15:21:22 +0900

    docs: Add comment

--- components/KbPlayer/logic.ts
+++ components/KbPlayer/logic.ts
@@ -1,15 +1,17 @@
 import { useEffect, useRef, useState } from 'react'
 
 const useLogic = () => {
-  const ref = useRef<HTMLVideoElement>(null)
+  const ref = useRef<HTMLVideoElement>(null) // Create ref object to use it in video element
 
-  const [progress, setProgress] = useState(0)
+  const [progress, setProgress] = useState(0) // A state of video playback status
 
+  // When mouse hover, video plays
   const onHover = async () => {
     const el = ref.current
     el && await el.play()
   }
 
+  // When mouse leaves this element, video stops and returns to the beginning
   const onUnHover = () => {
     const el = ref.current
     el && (() => {
@@ -20,10 +22,12 @@ const useLogic = () => {
 
   useEffect(() => {
     const el = ref.current
+
+    // When it is caused timeupdate event, set this state
     el && (() => {
       el.ontimeupdate = () => {
         setProgress(
-          Math.ceil(el.currentTime * 100 / el.duration)
+          Math.ceil(el.currentTime * 100 / el.duration) // As a percent
         )
       }
     })()
