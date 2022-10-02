a2a8df79ec7d344fcff86a28f3dac6533887e7ae
yaskou <anmitutarow@gmail.com>
2022-10-02 23:10:56 +0900

    fix: Add function name to fix eslint error of arrow function

--- components/KbPlayer/VideoPlayer.tsx
+++ components/KbPlayer/VideoPlayer.tsx
@@ -6,18 +6,19 @@ type Props = {
 }
 
 // Using ref object in props so use forwardRef
-// eslint-disable-next-line react/display-name
-const VideoPlayer = forwardRef<HTMLVideoElement, Props>((props, ref) => (
-  <Box
-    component="video"
-    ref={ref}
-    {...props}
-    maxWidth="100%"
-    maxHeight="100%"
-    muted
-    playsInline // Fix safari bug
-    loop // During mouse hover, video loops
-  />
-))
+const VideoPlayer = forwardRef<HTMLVideoElement, Props>(function VideoPlayer(props, ref) {
+  return (
+    <Box
+      component="video"
+      ref={ref}
+      {...props}
+      maxWidth="100%"
+      maxHeight="100%"
+      muted
+      playsInline // Fix safari bug
+      loop // During mouse hover, video loops
+    />
+  )
+})
 
 export default VideoPlayer
